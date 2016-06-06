af667026b2da02edf04ecec5ba87931ef7d4fb4b
David G. Simmons <santafen@mac.com>
2016-06-06 18:55:35 -0400

    Backward compatibility for Blinky
    Define LED_BLINK_PIN appropriately for Blinky

--- hw/bsp/stm32f3discovery/include/bsp/bsp.h
+++ hw/bsp/stm32f3discovery/include/bsp/bsp.h
@@ -52,6 +52,8 @@ extern uint8_t _ccram_start;
 #define LED_BLINK_PIN_7   (78)
 #define LED_BLINK_PIN_8   (79)
 
+#define LED_BLINK_PIN LED_BLINK_PIN_1
+
 /* UART ports */
 #define UART_CNT        1
 #define CONSOLE_UART    0
